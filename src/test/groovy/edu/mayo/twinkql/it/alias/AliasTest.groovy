bd7c8cf4813c01b0b62a25834d326c5a19e02ce9
Kevin Peterson <kevin.peterson@mayo.edu>
2012-06-11 22:00:19 -0500

    Added namespace alias test.

--- src/test/groovy/edu/mayo/twinkql/it/alias/AliasTest.groovy
+++ src/test/groovy/edu/mayo/twinkql/it/alias/AliasTest.groovy
@@ -8,6 +8,7 @@ import org.junit.Test
 import edu.mayo.twinkql.context.ConfigBuilder
 import edu.mayo.twinkql.context.TwinkqlContextFactory
 import edu.mayo.twinkql.model.AliasDefinition
+import edu.mayo.twinkql.model.NamespaceDefinition
 import edu.mayo.twinkql.template.TwinkqlTemplateFactory
 
 class AliasTest {
@@ -30,4 +31,27 @@ class AliasTest {
 		
 		assertTrue tetris.comments.size() > 1
 	}
+	
+	@Test
+	void TestAliasAndNamespaces(){
+		def factory = new TwinkqlContextFactory(
+				"http://dbpedia.org/sparql",
+				"classpath:edu/mayo/twinkql/it/alias/*.xml")
+		
+		def configBuilder = new ConfigBuilder().
+			addAlias(new AliasDefinition(id:"tetris", type:"edu.mayo.twinkql.it.alias.Tetris")).
+			addNamespace(new NamespaceDefinition(prefix:"rdfs", uri:"http://www.w3.org/2000/01/rdf-schema#")).
+			addNamespace(new NamespaceDefinition(prefix:"dbpedia", uri:"http://dbpedia.org/resource/"))
+		
+		factory.setConfigBuilder(configBuilder)
+		
+		def template = new TwinkqlTemplateFactory(factory.getTwinkqlContext()).getTwinkqlTemplate()
+		
+		def tetris = template.selectForObject("alias", "getTetrisCommentsWithNamespaces", null, Tetris)
+	
+		assertTrue tetris instanceof Tetris
+		
+		assertTrue tetris.comments.size() > 1
+	}
+
 }
